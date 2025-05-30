10e0e2232a5512e488974b3b354be1eb73cf77f1
zdsbs <na>
2025-05-30 16:14:54 -0400

    TODO list

--- calculator_server.py
+++ calculator_server.py
@@ -7,6 +7,29 @@ import traceback
 
 mcp = FastMCP("stuff")
 
+#TODO: 
+# - storing in memory all the chat history
+# - - you might have to have an eye forward to printing (what metadata might you want to have in memory?)
+# - - - There are a few ways you could do this? 1) timestampts 2) you could mutate the history with a printed flag 3) you could have a history of what's been printed
+# - - - There's a data structure here to track what's been printed and what's not
+# - printing out the history so that we're not duplicating anything and not missing anything
+# - - you'll have to know what's been printed vs in the in memory history
+# - make it pretty
+# - - CHALLENGE: don't rerun the server all the time to see if you're making it pretty or not? Save off the history RAW so you can recontrsutct the objects. Then iterate on prettness.
+# - IN GENERAL KEEP IT CLEAN AND SIMPLE
+
+
+# today how do we test?
+# 1) We make the change
+# 2) We retart the server
+# 3) we restart the MCP tool
+# 4) tytpe in prompt one to get it to give suggestions
+# 5) we type in the prompt "store chat info"
+# 6) we see the output
+
+# when you test is it pretty? Do you need to go through steps 1-6?
+# NO you don't need to do that? You need to write out the objects to disk and then write a new script that will read them in and try to make it pretty
+
 @mcp.tool()
 def store_chat_info(model_name: str, conversation_history: list) -> str:
     """
